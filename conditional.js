ab38128ea5812174bc34fc0b1c2a4a989c099df3
rishithapamulapati21-eng <rishithapamulapati21@gmail.com>
2026-08-04 09:37:24 +0530

    First commit

---
+++ conditional.js
@@ -0,0 +1,16 @@
+let a=12;
+let b=15;
+let c=11;
+ if(a>b && a>c) {
+    document.write("a is the largest number");
+
+ }
+ else if (b>a && b>c){
+    document.write("b is the largest number");
+ }
+ else if(c>a && c>b){
+     document.write("c is the largest number");
+ }
+ else{
+    document.write("all numbers are equal");
+ }
